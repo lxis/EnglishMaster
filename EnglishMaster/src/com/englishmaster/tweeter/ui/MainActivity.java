9871a220511bfe36f79149e4cd65c55ee585a2fa
lxis <lxis@163.com>
2014-05-22 19:16:00 +0800

    + grade page

--- EnglishMaster/src/com/englishmaster/tweeter/ui/MainActivity.java
+++ EnglishMaster/src/com/englishmaster/tweeter/ui/MainActivity.java
@@ -9,10 +9,13 @@ import com.englishmaster.tweeter.server.service.TweeterService;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 
@@ -26,11 +29,43 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         listView = (ListView)findViewById(R.id.mainList);
         LoadData();
+        InitEvents();
     }
 
 
 
-    private void LoadData() {
+    private void InitEvents() {
+    	final Button buttonLearn = (Button)findViewById(R.id.buttonLearn);
+    	final Button buttonGrade = (Button)findViewById(R.id.buttonGrade);
+    	final ListView listLearn = (ListView)findViewById(R.id.mainList);
+    	final LinearLayout linearGrade = (LinearLayout)findViewById(R.id.linearGrade);
+    	buttonLearn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				buttonLearn.setTextColor(Color.rgb(140,210,50));
+				buttonGrade.setTextColor(Color.WHITE);
+				linearGrade.setVisibility(8);
+				listLearn.setVisibility(0);
+			}
+		});
+		
+    	buttonGrade.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				buttonGrade.setTextColor(Color.rgb(140,210,50));	
+				buttonLearn.setTextColor(Color.WHITE);
+				listLearn.setVisibility(8);
+				linearGrade.setVisibility(0);
+			}
+		});
+		
+	}
+
+
+
+	private void LoadData() {
 		// TODO Auto-generated method stub
     	new AsyncTask<String,Integer,ArrayList<Article>>(){
     		@Override
